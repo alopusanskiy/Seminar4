2cd27a8450e7e984022e7b0d1488e5f88c24aa23
«Ales» <alopusansky@gmail.com>
2023-06-26 20:07:07 +0300

    add solution task_001

--- task_001/Program.cs
+++ task_001/Program.cs
@@ -1,14 +1,25 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
 
-System.Console.WriteLine("Введите число");
 
-int num = new Random().Next(100, 1000);
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
 
-int result = num / 10;
+void MathPow(int A, int B)
+{
+    int result = A;
+    for (int i = 1; i < B; i++)
+    {
+        result *= A;
+    }
+    System.Console.WriteLine(result);
+}
 
-System.Console.WriteLine(num);
+int number = ReadInt("Введите число: ");
+int degree = ReadInt("Введите степень числа: ");
 
-System.Console.WriteLine(result%10);
+MathPow(number, degree);
